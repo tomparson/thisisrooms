18abf7b5d63fe6565e511b92810b5754f7d43333
Tom Parson <tom@workwithhuddle.com>
2025-12-31 16:19:21 +0000

    Adding collabs and newsletter form

--- resources/views/livewire/post.blade.php
+++ resources/views/livewire/post.blade.php
@@ -8,10 +8,13 @@
             <div class="prose prose-invert max-w-none">
                 {!! $content !!}
             </div>
-            <a href="/" class="opacity-50 hover:opacity-100 hover:translate-x-[-5px] transition flex items-center gap-2 mt-[100px]">
-                <i class="fa-solid fa-arrow-left"></i>
-                <span>back to releases + about</span>
-            </a>
+            <div class="mt-[100px]">
+                @include('components.newsletter')
+                <a href="/" class="opacity-50 hover:opacity-100 hover:translate-x-[-5px] transition flex items-center gap-2">
+                    <i class="fa-solid fa-arrow-left"></i>
+                    <span>back to releases + about</span>
+                </a>
+            </div>
         </div>
     </div>
 </main>
